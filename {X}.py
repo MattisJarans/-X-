d00e3dbc45e2e4e82c98f01207e228b5f023f69b
MattisJarans <matiss.jarans@gmail.com>
2024-12-05 19:45:32 +0200

    Add files via upload

---
+++ {X}.py
@@ -0,0 +1,16 @@
+for i in range (0,10):
+    print('**********')
+
+for i in range (0,4):
+    print('****')
+
+for i in range(1):
+    print('    *')
+for i in range(1):
+    print('   ***')
+for i in range(1):
+    print('  *****')
+for i in range(1):
+    print(' *******')
+for i in range(1):
+    print('*********')
